b9f502a7fbf1693fe89a37a7578af1ddd12ab6ef
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-10 02:15:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  return array.first if array.size == 1
+  str = array.shift
+  array.each do |item|
+    str << "," if array.size > 1
+    str << " and" if item == array.last
+    str << " #{item}"
+  end
+  str
 end
