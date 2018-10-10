9fa6288704b9d14d66105310a0e04b11797d885c
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-10 02:13:25 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,7 @@ require 'pry'
 
 def oxford_comma(array)
   return array.first if array.size == 1
-  puts "after shift, array has #{array.size} items"
   str = array.shift
-  puts "before loop, array has #{array.size} items"
-  # binding.pry
   array.each do |item|
     str << " and" if item == array.last
     str << " #{item}"   # this messes up first item each time
