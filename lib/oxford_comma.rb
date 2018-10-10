5d3eb6282fa61c483f02a42d91bb1d1d4155b408
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-10 01:54:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
 def oxford_comma(array)
-  str = array.shift
   return array.first if array.size == 1
+  str = array.shift
   array.each do |item|
     str << " and" if item == array.last
     str << " #{item}"   # this messes up first item each time
