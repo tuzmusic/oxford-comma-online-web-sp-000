bbfdd82a4891ac210b4d4d4f01d9f0cbcdb464d8
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-10 01:28:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,13 @@
 require 'pry'
 
 def oxford_comma(array)
+  str = ""
+  return array.first if array.size == 1
+  array.each do |item|
+    str << "and " if item == array.last
+    str << item
+    str << ", " if item != array.last
+  end
   ands = array.join(' and ')
 end
 
