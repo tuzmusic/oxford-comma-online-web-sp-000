f4376b26c0eff2076722ec493fe95fc11137494a
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-10 02:15:00 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   return array.first if array.size == 1
   str = array.shift
   array.each do |item|
-    str << "," if item != array.last
+    str << "," if array.size > 1
     str << " and" if item == array.last
     str << " #{item}"
   end
